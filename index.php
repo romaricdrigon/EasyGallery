ff7fc8944cafa4dbe08f47445475533e32bc5f9c
Romaric Drigon <romaric.drigon@gmail.com>
2012-10-20 16:44:55 +0200

    Code a bit "stricter".
    Ok with Php 5.4, with no warnings. jQuery version updated. Empty
    folders are not displayed anymore.

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	 */
 	
 	/* Config */
-	// do you want to use thumbs? 60px images, galleries will load faster (big pictures are not loaded at the same time than the page)
+	// do you want to use thumbnails? 60px images, galleries will load faster (big pictures are not loaded at the same time than the page)
 	// it'll degrade smartly if no thumb is found - but disabling it may allow you to save a little time if you don't have any thumb
 	$use_thumbs = TRUE;
 	
@@ -24,13 +24,13 @@
 	<title>EasyGallery</title>
 	<link type="text/css" href="style.css" rel="stylesheet" />
 	<!-- jQuery -->
-	<script type="text/javascript" src="lib/jquery-1.6.4.min.js"></script>
+	<script type="text/javascript" src="lib/jquery.min.js"></script>
 	<!-- Galleria (jQuery plugin) -->
 	<script type="text/javascript" src="galleria/galleria-1.2.5.min.js"></script>
 	<script type="text/javascript" src="galleria/themes/classic/galleria.classic.min.js"></script>
 	<script type="text/javascript" src="galleria/plugins/history/galleria.history.min.js"></script>
-	<!-- custom script -->
-	<script type="text/javascript" src="easygallery.js"></script>	
+	<!-- our custom script -->
+	<script type="text/javascript" src="easygallery.js"></script>
 </head>
 <body>
 	<div class="main" id="main">
@@ -40,16 +40,11 @@
 			<!-- link to images - will be displayed if Javascript is disabled -->
 			<?php
 				// show link to images
-				foreach ($list['picture'] as $pic)
-				{
-					if (isset($pic['big']) && ($pic['big'] !== 'thumbnail.jpg')) // we check if the big picture exists
-					{
-						if (($use_thumbs === TRUE) && (isset($pic['thumb'])))
-						{
+				foreach ($list['picture'] as $pic) {
+					if (isset($pic['big']) && ($pic['big'] !== 'thumbnail.jpg')) { // we check if the big picture exists
+						if (($use_thumbs === TRUE) && (isset($pic['thumb']))) {
 							echo '<a href="'.$dir.'/'.$pic['big'].'" rel="'.$dir.'/'.$pic['thumb'].'">'.$pic['big'].'</a><br />'."\n";
-						}
-						else
-						{
+						} else {
 							// if no thumb is provided, Galleria will use the big picture automatically
 							echo '<a href="'.$dir.'/'.$pic['big'].'">'.$pic['big'].'</a><br />'."\n";
 						}
@@ -58,25 +53,22 @@
 			?>
 		</div>
 		<?php 
-			if (sizeof($list['folder']) != 0):
+			if (isset($list['folder']) && sizeof($list['folder']) !== 0):
 		?>
 			<div class="gallery_list">
 				<h3>Sous-galeries</h3>
 				<?php
 					// show link to images
-					foreach ($list['folder'] as $fol)
-					{
+					foreach ($list['folder'] as $fol) {
 						// test to avoid one more slash	
 						$path = ($subdir=='')?$fol:$subdir.'/'.$fol;
 						
-						$html = '<a href="?gallery='.gallery_link($path).'" title="'.$fol.'">'; // url encode, be prudent
-						$html .= '<div class="thumb" style="background-image: url(\''.$dir.'/'.$fol.'/'.get_thumbnail($dir.'/'.$fol).'\');"';
-						$html .= '><div class="text">'.$fol.'</div></div></a>'."\n";
-						
-						echo $html;
+						echo    '<a href="?gallery='.gallery_link($path).'" title="'.$fol.'">'.
+                                '<div class="thumb" style="background-image: url(\''.$dir.'/'.$fol.'/'.get_thumbnail($dir.'/'.$fol).'\');">'.
+						        '<div class="text">'.$fol.'</div></div></a>'."\n";
 					}
 				?>
-				<br clear="all" /><!-- the famous infamous trick -->
+				<br clear="all" />
 			</div>
 		<?php 
 			endif;
@@ -84,13 +76,13 @@
 	<div class="footer">
 		<?php 
 			// display the comment only if there's a gallery
-			if (sizeof($list['picture']) != 0):
+			if (isset($list['picture']) && sizeof($list['picture']) !== 0):
 		?>
 			<span class="white">Appuyer sur la touche "Entr&eacute;e" du clavier pour d&eacute;marrer le diaporama, "Esc" pour le quitter.</span><br />
 		<?php 
 			endif;
 		?>
-		EasyGallery, 2011, <a href="http://github.com/romaricdrigon/" target="_blank">http://github.com/romaricdrigon/</a>
+		EasyGallery, 2012, <a href="http://github.com/romaricdrigon/" target="_blank">http://github.com/romaricdrigon/</a>
 	</div>
 	</div> <!-- end main -->
 	<!-- finally, we load galleria -->
@@ -100,13 +92,12 @@
 		    height: 700,
 		    width: 960,
 		    debug: false,
-		    // we use a custom source, because img will all load on startup it's crappy, and json maybe not work if javascript is disabled
+		    // we use a custom source, because img will all load on startup it's crappy, and json may not work if javascript is disabled
 		    dataSelector: "a",
 		    dataConfig: function(a) {
 		        return {
 		            <?php
-		            	if ($use_thumbs === TRUE)
-						{
+		            	if ($use_thumbs === TRUE) {
 							echo 'thumb: $(a).attr(\'rel\'),'."\n";
 						}
 		            ?>
--- lister.php
+++ lister.php
@@ -13,27 +13,21 @@
 	 *  - dir name {string}, with no slashes
 	 */
 
- 	// we can't define an array as const - so we'll accede this var via super-global
- 	// and, oh, yes it's dirty, but so damn fast to code
-	$extensions = array('jpg', 'JPG', 'jpeg', 'JPEG', 'png', 'gif'); // allowed files extensions
+    // allowed files extensions
+	$extensions = array('jpg', 'JPG', 'jpeg', 'JPEG', 'png', 'PNG', 'gif', 'GIF');
 
 //  list images in a given directory
 function lister($dir, $use_thumbs)
 {
-
 	// test if dir exists before
-	if (is_dir($dir))
-	{
+	if (is_dir($dir)) {
 		$files = scandir($dir);
-	}
-	else
-	{
+	} else {
 		echo 'Dossier introuvable';
-		return;
+		return '';
 	}
 	
-	if ($files == FALSE)
-	{
+	if ($files == FALSE) {
 		echo 'Unable to open folder';
 	}
 	
@@ -43,89 +37,69 @@ function lister($dir, $use_thumbs)
 	$files = array_diff($files, array('.','..')); // array_diff to remove . and ..
 	sort($files, SORT_STRING); // sort, important for next step ; make sure everything is seen as strings
 	
-	foreach ($files as $file)
-	{
+	foreach ($files as $file) {
 		// careful, you have to call is_dir or is_file on the full path!
 		$f = $dir.'/'.$file;
 
-		if (is_dir($f) === TRUE) // directory
-		{
-			$content['folder'][] = $file;
-		}
-		else if (is_file($f) === TRUE && in_array(pathinfo($file, PATHINFO_EXTENSION), $extensions)) // file
-		{
+		if (is_dir($f) === TRUE && sizeof(scandir($f)) > 2) { // directory, not empty (2 items by default : . and ..)
+            $content['folder'][] = $file;
+		} else if (is_file($f) === TRUE && in_array(pathinfo($file, PATHINFO_EXTENSION), $extensions)) { // file
 			// we consider this script will not be used by idiots, so we don't re-check mime type
-			if (($use_thumbs === TRUE) && (strpos($file, '_thumb') != FALSE))
-			{
+			if (($use_thumbs === TRUE) && (strpos($file, '_thumb') != FALSE)) {
 				// it's a thumbnail - using str_replace and an associative array allow to match big picture & thumb
 				$content['picture'][str_replace('_thumb', '', $file)]['thumb'] = $file;
-			}
-			else
-			{
+			} else {
 				$content['picture'][$file]['big'] = $file; // otherwise it's a big one
 			}
 		}
-		// we elude symlinks
+		// we elude symlinks, to avoid loops
 	}
 	
 	return $content;
 }
 
 // return thumbnail representing a directory
-// V2 : recursivity FTW
 // param : dir : folder that must represented by a thumbnail
 function get_thumbnail($dir)
 {
-	global $extensions; // access defined file extensions list
+	$list = lister($dir, FALSE); // we don't care about thumbs, so save some time
 	
-	$list = lister($dir, FALSE); // we d'ont care about thumbs, so save some time
-	
-	if (isset($list['picture']['thumbnail.jpg']) === TRUE)
-	{
+	if (isset($list['picture']['thumbnail.jpg']) === TRUE) {
 		return 'thumbnail.jpg';
-	}
-	// get first picture file in the folder
-	else if ((isset($list['picture']) === TRUE) && (sizeof($list['picture']) > 0))
-	{
-		//reset($list['picture']); // does not seems necessary here
+	} else if ((isset($list['picture']) === TRUE) && (sizeof($list['picture']) > 0)) { // get first picture file in the folder
 		$key = key($list['picture']); // get current value
 		return $list['picture'][$key]['big'];
-	}
-	// search in sub folders
-	else if ((isset($list['folder']) === TRUE) && (sizeof($list['folder']) > 0))
-	{
-		foreach ($list['folder'] as $a_folder)
-		{
-			// recursivity, yeah!
+	} else if ((isset($list['folder']) === TRUE) && (sizeof($list['folder']) > 0)) { // search in sub folders
+		foreach ($list['folder'] as $a_folder) {
+			// recursivity FTW
 			$a_folder_thumb = get_thumbnail($dir.'/'.$a_folder);
 			
-			if ($a_folder_thumb !== '')
-			{
+			if ($a_folder_thumb !== '') {
 				return $a_folder.'/'.$a_folder_thumb;
 			}
 		}
 	}
 	
 	return ''; // nothing was found
-	
 }
 
 // return the folder from GET param
 function get_folder()
 {
+	if (! isset($_GET['gallery'])) {
+		return ''; // means root
+	}
+	
 	$folder = $_GET['gallery']; // $_GET is automatically url-decoded - and backslashes added
 	$folder = stripslashes($folder); // remove theses backslashes
 	
-	if (stripos($folder, '..') !== FALSE) // strict test (can return 0 for "position 0")
-	{
+	if (stripos($folder, '..') !== FALSE) {
 		//die("Forbidden directory"); // don't fuck with me bro
 		return ''; // be nice, just return to root
 	}
 	
 	// remove ending slash
-	$folder = no_slash($folder);
-	
-	return $folder;
+	return no_slash($folder);
 }
 
 // display a path link
@@ -135,18 +109,18 @@ function get_folder()
 function show_path($root, $dir)
 {
 	echo '<a href="?gallery=/" title="Index">Index</a>';
+    $link = '';
+    $full_path = '';
 	
-	if ($dir != '')
-	{	
+	if ($dir != '') {
 		$path = explode('/', $dir);
 		
-		foreach ($path as $step)
-		{
+		foreach ($path as $step) {
 			$link .= $step.'/';
 			$step = htmlentities($step, ENT_QUOTES, 'UTF-8'); // html encode to take care of probable shit, watch out we're in UTF-8 for folders
 			
 			// last step, assemble and apply gallery_link
-			$full_path .= ' &gt; <a href="?gallery='.gallery_link($link).'" title="'.$step.'">'.$step.'</a>';
+			$full_path .= ' / <a href="?gallery='.gallery_link($link).'" title="'.$step.'">'.$step.'</a>';
 		}		
 	}
 	
